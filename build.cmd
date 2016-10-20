f46ad31933a52cada911cae8e705d5c00c29321b
Josh Goldberg <josh@fullscreenmario.com>
2016-10-20 15:28:18 -0700

    Bumped to tslint 3.15.1

--- build.cmd
+++ build.cmd
@@ -1,10 +1,10 @@
 @echo off
 
-echo npm uninstall tslint
-call npm uninstall tslint
+echo npm uninstall tslint typescript
+call npm uninstall tslint typescript
 
-echo npm install tslint
-call npm install tslint
+echo npm install tslint typescript
+call npm install tslint typescript
 
 echo nuget pack tslint.nuspec
 call nuget pack tslint.nuspec
